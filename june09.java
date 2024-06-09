0d6b09981296b192b1449db0ac956285b2090c1e
Aakash Shalini <saakash2k4@gmail.com>
2024-06-09 21:27:50 +0530

    soln. added for june9

---
+++ june09.java
@@ -0,0 +1,18 @@
+public class june09 {
+ public static int subArrayDivByK(int[] nums, int k) {
+    int n = nums.length;
+    int prefixMod = 0, result = 0;
+
+    int[] modGroups = new int[k];
+
+    modGroups[0] = 1;
+
+    for(int num: nums) {
+      prefixMod = (prefixMod + num % k + k) % k;
+
+      result += modGroups[prefixMod];
+      modGroups[prefixMod]++;
+    }
+    return result;
+  }
+}
